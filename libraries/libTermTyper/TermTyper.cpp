2413625344954b5c174d1ab590cd307bb96726e3
Jaret Screws <jaretscrews@gmail.com>
2021-03-16 16:19:21 -0400

    Boilerplate for ncurses

---
+++ libraries/libTermTyper/TermTyper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jaret on 3/16/21.
+//
+
+#include "TermTyper.h"
---
+++ libraries/libTermTyper/TermTyper.h
@@ -0,0 +1,14 @@
+//
+// Created by jaret on 3/16/21.
+//
+
+#ifndef TERMTYPER_TERMTYPER_H
+#define TERMTYPER_TERMTYPER_H
+
+
+class TermTyper {
+
+};
+
+
+#endif //TERMTYPER_TERMTYPER_H
